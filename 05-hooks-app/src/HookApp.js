3bc3223956dc66ebe8098365f485bda1212aa5e8
Ocelopilli <angeljpa95@gmail.com>
2021-10-13 09:36:58 -0500

    inicio de 05-hooks

---
+++ 05-hooks-app/src/HookApp.js
@@ -0,0 +1,13 @@
+import CounterApp from "./components/01-useState/CounterApp";
+
+const HookApp = () => {
+  return (
+    <div>
+      <h1>Hooks</h1>
+      <br />
+      <CounterApp />
+    </div>
+  );
+};
+
+export default HookApp;
---
+++ 05-hooks-app/src/components/01-useState/CounterApp.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import "./counter.css";
+
+const CounterApp = () => {
+  const [state, setState] = useState({
+    counter1: 10,
+    counter2: 20,
+  });
+
+  const { counter1, counter2 } = state;
+
+  return (
+    <>
+      <h2>Counter1 {counter1}</h2>
+      <h2>Counter2 {counter2}</h2>
+      <hr />
+
+      <button
+        className="btn btn-primary"
+        onClick={() => {
+          setState({ ...state, counter1: counter1 + 1 });
+        }}
+      >
+        +1
+      </button>
+    </>
+  );
+};
+
+export default CounterApp;
