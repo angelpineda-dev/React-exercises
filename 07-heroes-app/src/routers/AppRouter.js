8e676cad8c14ecac4678b1b2950abcbffe9461c8
Ocelopilli <angeljpa95@gmail.com>
2021-10-22 11:58:11 -0500

    rutas privadas

--- 07-heroes-app/src/routers/AppRouter.js
+++ 07-heroes-app/src/routers/AppRouter.js
@@ -1,13 +1,22 @@
+import { useContext } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { AuthContext } from "../auth/AuthContext";
 import LoginScreen from "../components/login/LoginScreen";
 import DashboardRoutes from "./DashboardRoutes";
+import PrivateRoute from "./PrivateRoute";
 
 export const AppRouter = () => {
+  const { user } = useContext(AuthContext);
+
   return (
     <Router>
       <Switch>
         <Route exact path="/login" component={LoginScreen} />
-        <Route path="/" component={DashboardRoutes} />
+        <PrivateRoute
+          isAuth={user.logged}
+          path="/"
+          component={DashboardRoutes}
+        />
       </Switch>
     </Router>
   );
