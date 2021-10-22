ac592be477c047ead7e782ee126df41fd214085a
Ocelopilli <angeljpa95@gmail.com>
2021-10-22 15:29:39 -0500

    tests - AppRouter

--- 07-heroes-app/src/tests/routers/PrivateRoute.test.js
+++ 07-heroes-app/src/tests/routers/PrivateRoute.test.js
@@ -17,13 +17,22 @@ describe('Pruebas en <PrivateRoute/>', () => {
     <MemoryRouter>
       <PrivateRoute isAuth={true} component={ () => <span>Estoy Listo!</span> } {...props} />
     </MemoryRouter>
-    
     );
 
     expect(wrapper.find('span').exists()).toBe(true)
     expect(localStorage.setItem).toHaveBeenCalledWith("lastPath", "/marvel")
   });
 
+  test('debe de bloquear el componente si no esta autenticado', () => {
+    const wrapper = mount(
+      <MemoryRouter>
+        <PrivateRoute isAuth={false} component={ () => <span>Estoy Listo!</span> } {...props} />
+      </MemoryRouter>
+    );
 
+     expect(wrapper.find('span').exists()).toBe(false)
+    expect(localStorage.setItem).toHaveBeenCalledWith("lastPath", "/marvel")
+  })
+  
   
 });
