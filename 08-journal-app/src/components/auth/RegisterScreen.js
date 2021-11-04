3e6c939af1d83eedc069676d74358d6793e98e38
Angel_Pineda <angeljpa95@gmail.com>
2021-11-04 15:29:08 -0600

    08 - uiReducer, acciones

--- 08-journal-app/src/components/auth/RegisterScreen.js
+++ 08-journal-app/src/components/auth/RegisterScreen.js
@@ -1,13 +1,18 @@
 import { Link } from "react-router-dom";
 import { useForm } from "../../hooks/useForm";
+import { useDispatch } from "react-redux";
+import { removeError, setError } from "../../actions/ui";
+import validator from "validator";
+
+const initialForm = {
+  name: "Angel",
+  email: "correo@dominio.com",
+  password: "123456",
+  password2: "123456",
+};
 
 const RegisterScreen = () => {
-  const initialForm = {
-    name: "Angel",
-    email: "correo@dominio.com",
-    password: "123456",
-    password2: "123456",
-  };
+  const dispatch = useDispatch();
 
   const [formValues, handleInputChange] = useForm(initialForm);
 
@@ -16,16 +21,42 @@ const RegisterScreen = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    console.log(name, email, password, password2);
+    if (isFormValid()) {
+      console.log("Formulario correcto");
+    }
   };
 
   const isFormValid = () => {
-    //TODO:
+    if (name.trim().length === 0) {
+      const errMsg = "Name is required";
+      dispatch(setError(errMsg));
+
+      return false;
+    } else if (!validator.isEmail(email)) {
+      const errMsg = "Email is not valid";
+
+      dispatch(setError(errMsg));
+      return false;
+    } else if (password !== password2 || password.length < 5) {
+      const errMsg =
+        "Password should be at least 6 character and match each other.";
+
+      dispatch(setError(errMsg));
+
+      return false;
+    }
+
+    dispatch(removeError());
+
+    return true;
   };
 
   return (
     <div>
       <h3 className="auth__title mb-5">Register</h3>
+
+      <div className="auth__alert-error">Hola Mundo</div>
+
       <form onSubmit={handleSubmit}>
         <input
           type="text"
--- 08-journal-app/src/store/store.js
+++ 08-journal-app/src/store/store.js
@@ -1,11 +1,13 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import { authReducer } from "../reducers/authReducer";
+import { uiReducer } from "../reducers/uiReducer";
 
 // El store solo puede recibir un reducer, por eso se necesitan combinar antes de ser recibidos.
 
 const reducers = combineReducers({
   auth: authReducer,
+  ui: uiReducer,
 });
 
 const composeEnhancers =
